a6ea8601c70e319674fff268d3d8bd6d02d357e9
zoewrenn <zoemirk@yahoo.com>
2023-08-16 19:21:46 -0700

    Changed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import './App.css';
 import AdvancedSearchComponent from './components/AdvancedSearchComponent';
 import CreateComponent from './components/CreateComponent';
 import SongList from './components/SongList';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 function App() {
     return (
         <Router>
             <div className="App">
                 {/* Header with navigation links */}
-                <header className="App-header">
-                    <nav>
-                        <ul>
-                            <li><Link to="/">Search</Link></li>
-                            <li><Link to="/create">Create</Link></li>
-                            <li><Link to="/songs">Songs</Link></li>
-                        </ul>
-                    </nav>
-                </header>
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <Container>
+                        <Navbar.Brand href="#">Your App Name</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link as={Link} to="/">Search</Nav.Link>
+                                <Nav.Link as={Link} to="/create">Create</Nav.Link>
+                                <Nav.Link as={Link} to="/songs">Songs</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
 
                 {/* Routes */}
-                <Routes>
-                    <Route path="/" element={<AdvancedSearchComponent />} />
-                    <Route path="/create" element={<CreateComponent />} />
-                    <Route path="/songs" element={<SongListPage />} />
-                </Routes>
+                <Container className="mt-4">
+                    <Routes>
+                        <Route path="/" element={<AdvancedSearchComponent />} />
+                        <Route path="/create" element={<CreateComponent />} />
+                        <Route path="/songs" element={<SongListPage />} />
+                    </Routes>
+                </Container>
             </div>
         </Router>
     );
